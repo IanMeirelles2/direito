ac71514cb4be1af15266e7e9e50e11af067ba4dd
Ian Meirelles Durães <162380721+IanMeirelles2@users.noreply.github.com>
2024-06-10 16:57:12 -0300

    pronto

--- operacoes.py
+++ operacoes.py
@@ -1,14 +1,12 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 #lendo o arquivo csv
 df = pd.read_csv('/workspaces/direito/database/db_updated.csv', sep=',', encoding='utf-8')
 
-#agrupar por por nome e contar o número de casos em cada um
+#agrupar por nome e contar o número de casos em cada um
 cases_by_name = df.groupby('Nome do Advogado Principal').size().reset_index(name='Número de Casos')
 
-#criar uma lista com os nomes dos advogados que possuem mais de 10 casos
+#criar uma lista com somente os nomes dos advogados que possuem mais de 10 casos
 cases_choices = []
 for name in cases_by_name['Nome do Advogado Principal']:    
     cases_separate = cases_by_name[cases_by_name['Nome do Advogado Principal'] == name]
@@ -20,5 +18,6 @@ for nm in cases_choices:
     cases = df.loc[df['Nome do Advogado Principal'] == nm]
     actions = cases['Ação'].value_counts().reset_index(name='Número de Casos')
     cnpj = cases['CNPJ do Réu'].value_counts().reset_index(name='Número de Casos')
+    #print das informações escolhidas como mais importante(Ações e Cnpj) de cada um dos advogados com mais casos de forma organizada
     print(f"\n{nm.upper()} \n\nAções: \n{actions}\n\nCNPJ: \n{cnpj}")
 
